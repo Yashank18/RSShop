5052fe5f43fb0899fd5c52e0cb7df37e90ecbdfe
Yashank18 <yashank7696@gmail.com>
2021-01-14 21:53:22 +0530

    Landing Page Product Slider

--- rsshoppie/client/src/components/views/LandingPage/LandingPage.js
+++ rsshoppie/client/src/components/views/LandingPage/LandingPage.js
@@ -2,6 +2,7 @@ import React,{useEffect,useState} from 'react'
 import Axios from 'axios';
 import {Icon,Row,Button, Col, Card} from 'antd';
 import Meta from 'antd/lib/card/Meta';
+import ImageSlider from '../../utils/ImageSlider'
 
 function LandingPage() {
 
@@ -25,7 +26,7 @@ function LandingPage() {
 
     const rendercards = Products.map((product,index)=>{
             return <Col lg={6} md={8} xs={24}>
-                <Card hoverable={true} cover>
+                <Card hoverable={true} cover={<ImageSlider images={product.images}/>}>
                     <Meta title={product.title} description={`$${product.price}`}>
                     </Meta>
                 </Card>
